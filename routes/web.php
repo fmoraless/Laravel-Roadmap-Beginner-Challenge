2aabfd78366fd2f7c3a894d3826694992a250738
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-24 00:26:20 -0400

    category routes

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/categories', 'CategoryController')->names('categories');
+Route::resource('/categories', 'CategoryController')->names('categories')->middleware('auth');
